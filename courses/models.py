9cc6011d50850f19ac625331bbaec4f93943b54f
Wetal <margenshteynv@gmail.com>
2024-11-07 18:51:49 +0300

    add models Course and Lesson

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=250, verbose_name='title')
+    preview = models.ImageField(upload_to="courses/previews/", verbose_name="previews", blank=True, null=True,
+                               help_text="Загрузите превью")
+    description = models.TextField(verbose_name='description')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=250, verbose_name='title')
+    description = models.TextField(verbose_name='description')
+    preview = models.ImageField(upload_to="courses/lessons/previews/", verbose_name="previews", blank=True, null=True,
+                                help_text="Загрузите превью")
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        verbose_name="Курс к которому относится урок",
+        help_text="Выберете курс, к которому относится урок",
+        blank=True,
+        null=True,
+        related_name="course",
+    )

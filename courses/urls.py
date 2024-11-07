1690898c64a105b03dd7b8058d596f3573565a2f
Wetal <margenshteynv@gmail.com>
2024-11-07 19:25:00 +0300

    generic CRUD for model Lesson

--- courses/urls.py
+++ courses/urls.py
@@ -1,13 +1,21 @@
+from django.urls import path
+
 from courses.apps import CoursesConfig
 from rest_framework.routers import DefaultRouter
 
-from courses.views import CourseViewSet
+from courses.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = CoursesConfig.name
 
-
 router = DefaultRouter()
 router.register(r'courses', viewset=CourseViewSet, basename='courses')
-urlpatterns = [
 
+urlpatterns = [
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_get'),
+    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
 ] + router.urls
+
